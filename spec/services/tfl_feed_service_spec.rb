8d688ef8a2a09185fee8613e732659684c0f73ac
doutatsu <doutatsu@me.com>
2016-08-17 22:52:44 +0100

    [Testing] Extra tests

--- spec/services/tfl_feed_service_spec.rb
+++ spec/services/tfl_feed_service_spec.rb
@@ -2,10 +2,13 @@ require './app/services/tfl_feed_service'
 
 describe TflFeedService do
   describe '#get_disruptions' do
-    it "should return all the disruptions given an xml" do
-      tfl_service = TflFeedService.new('https://data.tfl.gov.uk/tfl/syndication/feeds/tims_feed.xml')
+    let(:disruptions) { TflFeedService.new('https://data.tfl.gov.uk/tfl/syndication/feeds/tims_feed.xml').get_disruptions }
+    it "should return an array given an url of xml" do
+      expect(disruptions.kind_of?(Array)).to eq(true)
+    end
 
-      expect(tfl_service.get_disruptions[0]).to eq({:lng=>"-0.066583", :lat=>"51.442505"})
+    it "should contain hashes of locations" do
+      expect(disruptions.map(&:keys).all? {|loc| loc == [:lng, :lat]}).to eq(true)
     end
   end
 end
