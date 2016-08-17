be9cac15486dd12d1474024d97df0ddd71d79005
doutatsu <doutatsu@me.com>
2016-08-17 19:08:28 +0100

    [Refactoring] Center initial map on London + rename website title

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,7 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 51.500152, lng: -0.126236},
+    zoom: 12
   });
 }
